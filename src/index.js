ceb0b3b67e5497a4b437ef53d17fe7210557d554
ursulafahle <161364127+ursulafahle@users.noreply.github.com>
2024-04-21 13:28:54 +0200

    Search Functionality Using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function refreshweather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "14600o81bf492c92cb64c3ab43ad1f9t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshweather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Münster");
